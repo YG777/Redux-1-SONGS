450a85735c4c0db77f491e29a8157656a504585b
YG777 <katyake@gmail.com>
2020-06-12 14:53:03 +0100

    completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import SongList from './components/songList';
 
 function App() {
   return (
     <div className="App">
-      song app
+   <SongList/>
     </div>
   );
 }
